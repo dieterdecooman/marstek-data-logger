5955e4c05554df524eee0cbc1178c00bbef8df44
dieterdecooman <73192452+dieterdecooman@users.noreply.github.com>
2025-09-17 23:38:04 +0200

    Update streamlit_marstek.py

--- streamlit_marstek.py
+++ streamlit_marstek.py
@@ -161,7 +161,9 @@ fig = px.line(
 
 fig.update_layout(transition_duration=300,autosize=True)
 fig.update_traces(mode="lines+markers", marker=dict(size=4))
-fig.update_yaxes(range=[0, 100])
+fig.update_yaxes(range=[0, 100],autorange=True)
+fig.update_xaxes(autorange=True)
+
 
 st.plotly_chart(fig, use_container_width=True,config={"responsive": True}  )
 
